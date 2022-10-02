867e831afaec12af53d78420879b8b54bb752924
Alex <pidtserkovnyy92@mail.ru>
2022-10-02 23:55:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// "use sctrict"
+
+// //alert("Hello");
+
+// //const result = confirm("Ты сдесь пидор?");
+// //console.log(result);
+
+// //  const answer = +prompt("Сколько вам лет?", "");
+// //  console.log(answer + 5);
+
+// const answers = [];
+
+// // answers[0] = prompt("Как вас зовут?", "");
+// // answers[1] = +prompt("Сколько вам лет?", "");
+// // answers[2] = prompt("Пидор ли ты?", "");
+// // answers[3] = prompt("А не пидор ли ты часом?", "");
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+
+
+//////////////
+
+// const category = "toys";
+
+// console.log("https://mail.ru/");
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
+    
